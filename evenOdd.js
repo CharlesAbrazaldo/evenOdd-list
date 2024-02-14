2585e6cae3b1c242d4af27b068dda98000bbe3b2
CharlesAbrazaldo <charles.abrazaldo@gmail.com>
2024-02-13 20:56:13 -0600

    updated list

---
+++ evenOdd.js
@@ -0,0 +1,25 @@
+<script>
+let usersWord = ""
+
+function updateWord(element) {
+  usersWord = element.value
+  }
+</script>
+// ------------
+let usersWord = ""
+let oddList = document.getElementById("odd")
+let evenList = document.getElementById("even")
+// ------------
+function handleSubmit() {
+  let newListItem = document.createElement('LI')
+  newListItem.innerText = usersWord
+
+  if(usersWord.length % 2 === 0) {
+    evenList.appendChild(newListItem)
+  } else {
+    oddList.appendChild(newListItem)
+    }
+  
+  usersWord = ""
+    document.getElementById("even-odd-form").reset()
+}
